dad4cce06357b48af134b4db0a69f26cff979a55
rivayama <tomozo16@gmail.com>
2016-03-16 20:50:38 +0900

    Normalize the answer text to make evaluation fair

--- rails/app/models/question.rb
+++ rails/app/models/question.rb
@@ -4,7 +4,16 @@ class Question < ActiveRecord::Base
   validates_presence_of :answer
 
   def is_correct?(submission)
-    answer == submission
+    normalize_answer(answer) == normalize_answer(submission)
+  end
+
+  private
+
+  def normalize_answer(str)
+    str.strip
+      .gsub(/\s{2,}/, ' ')
+      .downcase
+      .gsub(/(\d+)/) { |m| m.to_i.to_words }
   end
 
 end
